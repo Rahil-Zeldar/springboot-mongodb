8b66277662eba2c44e5ee3bee27ad0f61a932094
Rahil Zeldar <rahilkhan8322@gmail.com>
2025-12-28 16:37:15 +0530

    Created employees.html

--- src/main/java/com/example/springboot_mongodb/controller/EmployeeWebController.java
+++ src/main/java/com/example/springboot_mongodb/controller/EmployeeWebController.java
@@ -4,7 +4,6 @@ import com.example.springboot_mongodb.entity.Employee;
 import com.example.springboot_mongodb.entity.Projects;
 import com.example.springboot_mongodb.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
